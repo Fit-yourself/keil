452c88146169041bd35cb7792d53dbfffac0cf01
vseelove <vseelove@foxmail.com>
2021-11-06 20:26:27 +0800

    first commit

---
+++ RTE/_adc14_single_conversion_repeat_timera_source/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'adc14_single_conversion_repeat_timera_source' 
+ * Target:  'adc14_single_conversion_repeat_timera_source' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "msp.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
